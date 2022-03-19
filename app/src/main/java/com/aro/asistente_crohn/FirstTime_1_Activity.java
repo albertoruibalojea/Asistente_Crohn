9bcfbea524883fd945b9b6ea89c0351a93f44516
Alberto <alberto.ruibal.ojea@outlook.es>
2022-03-19 16:52:55 +0100

    fonts, colors

---
+++ app/src/main/java/com/aro/asistente_crohn/FirstTime_1_Activity.java
@@ -0,0 +1,14 @@
+package com.aro.asistente_crohn;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FirstTime_1_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first_time1);
+    }
+}
